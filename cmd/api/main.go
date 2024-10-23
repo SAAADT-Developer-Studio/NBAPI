48e67ea30afdd5c38770099d439939afc5d78cd9
marko154 <marko.gartnar777@gmail.com>
2024-10-23 20:22:10 +0200

    graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,60 @@
 package main
 
 import (
+	"NBAPI/internal/config"
 	"NBAPI/internal/server"
-	"fmt"
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	_ "github.com/joho/godotenv/autoload"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	logrus.Info("Starting server...")
+	log.Info("Starting server...")
 	server := server.NewServer()
-	logrus.Infof("Server listening on %s\n", server.Addr)
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+		log.Info("Shutting down...")
+		shutdownCtx, _ := context.WithTimeout(serverCtx, gracefulShutdownTimeout())
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+
+	// Run the server
+	log.Infof("Server listening on %s\n", server.Addr)
 	err := server.ListenAndServe()
-	if err != nil {
-		panic(fmt.Sprintf("cannot start server: %s", err))
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
 	}
+
+	// Wait for server context to be stopped
+	<-serverCtx.Done()
+}
+
+func gracefulShutdownTimeout() time.Duration {
+	if config.Config.AppEnv == "local" {
+		return 5 * time.Millisecond
+	}
+	return 30 * time.Second
 }
