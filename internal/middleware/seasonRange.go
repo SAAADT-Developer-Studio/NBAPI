e4eab1aed7807f790a42ea777fba0fccb567ee0a
marko154 <marko.gartnar777@gmail.com>
2024-11-14 03:56:15 +0100

    season range params validator

--- internal/middleware/seasonRange.go
+++ internal/middleware/seasonRange.go
@@ -9,6 +9,11 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type SeasonRangeParams struct {
+	SeasonFrom int `query:"seasonFrom" default:"1800"`
+	SeasonTo   int `query:"seasonTo" default:"2100"`
+}
+
 type SeasonRanges string
 
 const (
